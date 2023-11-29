dfc319fb0f920f0357a50078140e508ea82ef9a8
Dra-kt <dra_kt@outlook.com>
2023-11-29 16:51:29 +0800

    改进依赖安装

--- install_requirements.bat
+++ install_requirements.bat
@@ -1,4 +1,24 @@
+@echo off
+
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 cd/d "%~dp0"
-pip install -r requirements.txt -i https://pypi.doubanio.com/simple/
-pause
+chcp 65001 >NUL
+@title 安装依赖环境
+
+set CURR_PATH=%~dp0
+set VENV_PATH=%CURR_PATH%\venv\
+set LOCAL_PYTHON=%VENV_PATH%\Scripts\
+
+if not exist "%VENV_PATH%" (
+    echo 创建虚拟环境
+    python -m venv %VENV_PATH%
+)
+
+echo 开始安装依赖
+%LOCAL_PYTHON%\pip.exe install -r %CURR_PATH%\requirements.txt -i https://pypi.doubanio.com/simple/
+if %ERRORLEVEL% NEQ 0 (
+    echo 安装返回异常: %ERRORLEVEL%
+) else (
+    echo 安装完成，按任意键退出
+)
+pause > NUL
