a6553892e5be6d328d7048be280cde3d606d0662
Dra-kt <dra_kt@outlook.com>
2023-11-30 02:12:08 +0800

    align.bat适配虚拟环境

--- align.bat
+++ align.bat
@@ -1,4 +1,17 @@
+@echo off
+
+@REM 若无管理员权限则获取管理员权限
+net session 1>NUL 2>NUL && (
+goto as_admin
+)
+
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 cd/d "%~dp0"
-python align_angle.py
-pause
+
+:as_admin
+set CURR_PATH=%~dp0
+set VENV_PATH=%~dp0venv\
+set LOCAL_PYTHON=%VENV_PATH%\Scripts\
+
+%LOCAL_PYTHON%python.exe %CURR_PATH%align_angle.py
+pause > NUL
